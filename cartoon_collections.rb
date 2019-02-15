783767a23214755ae60438b1010e483750f8e995
LondonBishop <londonbishop@codecutters.co.uk>
2019-02-15 12:45:14 +0000

    solution using ruby way

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
+
+def roll_call_dwarves(arrIn)
+  arrIn.each_with_index { |dwarf, index| puts "#{index+1}. #{dwarf}" }
+  # code an argument here
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(arrIn)
+  arrIn.map! {|hero| hero.capitalize + "!" }
+
+  # code an argument here
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(arrCalls)
+  arrCalls.any? {|call| call.length > 4 }
+  # code an argument here
   # Your code here
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arrCheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  arrCheese.each { |element|
+    cheese_types.include?(element) ? theCheeseFound = element : nil
+  }
+
+  return theCheeseFound
+
 end
