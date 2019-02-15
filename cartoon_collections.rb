2ad2f96f3dd1e5b5f1590cc38f6da7f61dde1023
HARRY BHATT <bob@brahmingate.co.uk>
2019-02-15 11:19:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,36 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+
+def roll_call_dwarves(arrIn)
+  arrIn.each_with_index { |dwarf, index| puts "#{index+1}. #{dwarf}" }
+  # code an argument here
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(arrIn)
+  arrIn.map! {|hero| hero.capitalize + "!" }
+
+  # code an argument here
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(arrCalls)
+  arrCalls.any? {|call| call.length > 4 }
+  # code an argument here
   # Your code here
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arrCheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  counter = 0
+  cheeseFound = false
+
+  while cheeseFound != true || counter <= arrCheese.length-1 
+    cheeseFound = cheese_types.include?(arrCheese[counter])
+   # counter++
+  end
+  
+  if cheeseFound == true
+    arrCheese[counter]
+  end
+  
 end
